de7b86a8014f0b97f1152bbc333ac53e66d0cc1d
Patricia <pattricianunes@gmail.com>
2019-05-09 01:11:03 +0200

    heroku hand

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -8,34 +8,35 @@ import React from "react";
 class Weather extends React.Component{
     render(){
         return(
+		<div className="info">
             <div className="info-weather">
-                <div className="table-responsive">          
-                    <table className="table">
-                        <thead>
-                            <tr>
-                                <th>City</th>
-                                <th>Country</th>
-                                <th>Temperature</th>
-                                <th>Humidity</th>
-                                <th>Condition</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                {this.props.city && <td>{this.props.city}</td>}
-                                {this.props.country && <td>{this.props.country}</td>}
-                                {this.props.temperature && <td>{this.props.temperature} &#8451;</td>}
-                                {this.props.humidity && <td>{this.props.humidity}</td>}
-                                {this.props.description && <td>{this.props.description}</td>}
-                            </tr>
-                        </tbody>
-                    </table>
-                
-                </div>
+				<div className="table">
+					<table>
+						<thead>
+							<tr>
+								<th>City</th>
+								<th>Country</th>
+								<th>Temperature</th>
+								<th>Humidity</th>
+								<th>Condition</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								{this.props.city && <td>{this.props.city}</td>}
+								{this.props.country && <td>{this.props.country}</td>}
+								{this.props.temperature && <td>{this.props.temperature} &#8451;</td>}
+								{this.props.humidity && <td>{this.props.humidity}</td>}
+								{this.props.description && <td>{this.props.description}</td>}
+							</tr>
+						</tbody>
+					</table>
+				</div>
                 <div className="error-message">
                     {this.props.error && <p>{this.props.error}</p>}
                 </div>
             </div>
+		</div>
         );
     }
 }
