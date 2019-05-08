108b43c033977cb9f4da7a2a97d1a8d90d0e7f07
Patricia <pattricianunes@gmail.com>
2019-05-09 01:15:38 +0200

    new build

---
+++ docs/precache-manifest.595bed49bcf15e541062c40bc07db252.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4b89601c37bdfb5f851090e5c19ccea",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "cabdbd7fd7e05200a6bf",
+    "url": "/react-weather-app/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "b845e7ea1fe905741746",
+    "url": "/react-weather-app/static/css/main.ffcd8781.chunk.css"
+  },
+  {
+    "revision": "cabdbd7fd7e05200a6bf",
+    "url": "/react-weather-app/static/js/2.f464af3b.chunk.js"
+  },
+  {
+    "revision": "b845e7ea1fe905741746",
+    "url": "/react-weather-app/static/js/main.7d746b67.chunk.js"
+  },
+  {
+    "revision": "e736fc5812ad96aa92b3",
+    "url": "/react-weather-app/static/js/runtime~main.7e96bc80.js"
+  }
+]);
