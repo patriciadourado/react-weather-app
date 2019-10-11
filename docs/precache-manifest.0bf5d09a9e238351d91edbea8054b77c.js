027c8c1e11eabcb817437336376663b69f8bf9b3
Patricia <pattricianunes@gmail.com>
2019-10-11 06:10:50 +0200

    npm build

---
+++ docs/precache-manifest.0bf5d09a9e238351d91edbea8054b77c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aa4dc6a72e78e044e657fb7719f6851",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "c87fb27c69afda9b74ad",
+    "url": "/react-weather-app/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "f127859a5fa87ae93432",
+    "url": "/react-weather-app/static/css/main.779b36b0.chunk.css"
+  },
+  {
+    "revision": "c87fb27c69afda9b74ad",
+    "url": "/react-weather-app/static/js/2.38c32039.chunk.js"
+  },
+  {
+    "revision": "f127859a5fa87ae93432",
+    "url": "/react-weather-app/static/js/main.6f21c2b1.chunk.js"
+  },
+  {
+    "revision": "e736fc5812ad96aa92b3",
+    "url": "/react-weather-app/static/js/runtime~main.7e96bc80.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-weather-app/precache-manifest.c4c291ad3f10ecdc52c7a041e6bf51fc.js"
+  "/react-weather-app/precache-manifest.0bf5d09a9e238351d91edbea8054b77c.js"
 );
 
 self.addEventListener('message', (event) => {
